9971925c1e5b085a795980a6d48fc050a0a28639
NicholasKikoyo <nicholaskikoyo@gmail.com>
2021-01-29 13:21:16 +0100

    Initial commit

---
+++ src/Univeristy.java
@@ -0,0 +1,20 @@
+
+        import java.util.ArrayList;
+        import java.util.List;
+
+public class Univesity {
+
+    /*List<Student> students = new ArrayList<>();
+    List<Teacher> teachers = new ArrayList<>();
+    List<AdministativeStaff> staff = new ArrayList<>();*/
+
+    List<Person> universityPeople = new ArrayList<>();
+
+    /*public static void main(String[] args) {
+        Univesity univesity = new Univesity();
+        System.out.println(univesity.students.toString());
+    }
+*/
+
+
+}
